0b08ae6ec2bb1d09cdae04c43f9a716bc7754df1
Selina Jenifer <selinajenifer2000@gmail.com>
2025-04-10 11:51:36 +0530

    Tag Implemented (10/04/2025) - 1

---
+++ Pet-Store-Service/src/main/java/com/hcltech/dto/TagResponseDTO.java
@@ -0,0 +1,17 @@
+package com.hcltech.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class TagResponseDTO {
+
+    private Long tagId;
+
+    private String tagName;
+}
---
+++ Pet-Store-Service/src/main/java/com/hcltech/service/TagService.java
@@ -0,0 +1,13 @@
+package com.hcltech.service;
+
+import com.hcltech.dto.TagRequestDTO;
+import com.hcltech.dto.TagResponseDTO;
+
+import java.util.List;
+
+public interface TagService {
+    TagResponseDTO createTag(TagRequestDTO dto);
+    List<TagResponseDTO> getAllTags();
+    TagResponseDTO getTagById(Long id);
+    void deleteTag(Long id);
+}
